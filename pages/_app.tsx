45f48e4cb0f5304f417a13abf2821d9a668bd848
Mustafa Arda Düşova <127254301+mustcodes@users.noreply.github.com>
2024-09-14 14:09:55 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css';
 import 'antd/dist/antd.css';
-import '../styles/styles.css'
 import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
