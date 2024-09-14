cb4117692e90b131b13ea6fa34f2a6206e47f7a0
Mustafa Arda Düşova <127254301+mustcodes@users.noreply.github.com>
2024-09-14 13:23:44 +0300

    Update MarkdownSnippet.tsx

--- components/MarkdownSnippet.tsx
+++ components/MarkdownSnippet.tsx
@@ -1,4 +1,4 @@
-import { Input, Row, Col, Space, Typography } from 'antd';
+import { Input, Space, Typography } from 'antd';
 import React from 'react';
 import * as Constants from '../utils/Constants';
 
@@ -6,8 +6,10 @@ const { Text, Title } = Typography;
 const { TextArea } = Input;
 
 interface Props {
+
     username?: string;
 }
+
 
 export default function MarkdownSnippet(props: Props): JSX.Element | null {
     const { username } = props;
@@ -22,58 +24,29 @@ export default function MarkdownSnippet(props: Props): JSX.Element | null {
     const uniqueTracks = `![Alt text](${svgSrc}&unique={true|1|on|yes})`;
 
     return (
-        <>
-            <Row gutter={16}>
-                <Col xs={24} sm={24} md={24} lg={24} xl={24}>
-                    <Space className="vert-space" direction="vertical" size="small">
-                        <Title level={5}>{username} olarak giriş yapıldı.</Title>
-                        <Text>Markdown kod parçacığı:</Text>
-                        <TextArea className="markdown" autoSize readOnly value={markdownCode} />
-                        <Text>
-                            Özel sayı için (
-                            <b>
-                                {Constants.minCount} &#8804; &#123;Sayı&#125; &#8804; {Constants.maxCount}
-                            </b>
-                            ):
-                        </Text>
-                        <TextArea className="markdown" autoSize readOnly value={customCount} />
-                        <Text>
-                            Özel genişlik için (
-                            <b>
-                                {Constants.minWidth} &#8804; &#123;Genişlik&#125; &#8804; {Constants.maxWidth}
-                            </b>
-                            ):
-                        </Text>
-                        <TextArea className="markdown" autoSize readOnly value={customWidth} />
-                        <Text>Benzersiz parçalar için:</Text>
-                        <TextArea className="markdown" autoSize readOnly value={uniqueTracks} />
-                        <div className="svg-container">
-                            <img className="svg-preview" src={svgSrc} alt="SVG Preview" />
-                        </div>
-                    </Space>
-                </Col>
-            </Row>
-            <style jsx>{`
-                .markdown {
-                    font-family: monospace;
-                    margin-bottom: 8px !important;
-                    width: 100%;
-                }
-
-                /* Media queries for responsiveness on small screens */
-                @media (max-width: 768px) {
-                    .svg-preview {
-                        width: 100%; /* Scale SVG to fit container width on smaller screens */
-                        height: auto; /* Maintain aspect ratio */
-                    }
-                }
-
-                @media (max-width: 576px) {
-                    .markdown {
-                        font-size: 14px; /* Adjust font size for smaller screens */
-                    }
-                }
-            `}</style>
-        </>
+        <Space className="vert-space" direction="vertical" size="small">
+            <Title level={5}>{username} olarak giriş yapıldı.</Title>
+            <Text>Markdown kod parçacığı:</Text>
+            <TextArea className="markdown" autoSize readOnly value={markdownCode} />
+            <Text>
+            Özel sayı için (
+                <b>
+                    {Constants.minCount} &#8804; &#123;Sayı&#125; &#8804; {Constants.maxCount}
+                </b>
+                ):
+            </Text>
+            <TextArea className="markdown" autoSize readOnly value={customCount} />
+            <Text>
+            Özel genişlik için (
+                <b>
+                    {Constants.minWidth} &#8804; &#123;Genişlik&#125; &#8804; {Constants.maxWidth}
+                </b>
+                ):
+            </Text>
+            <TextArea className="markdown" autoSize readOnly value={customWidth} />
+            <Text>Benzersiz parçalar için:</Text>
+            <TextArea className="markdown" autoSize readOnly value={uniqueTracks} />
+            <object type="image/svg+xml" data={svgSrc}></object>
+        </Space>
     );
 }
